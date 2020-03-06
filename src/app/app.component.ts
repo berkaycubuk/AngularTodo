450bccdb42f7130d990575248dfaa6e0b60e614c
Berkay Çubuk <berkaycubuk@gmail.com>
2020-03-06 16:44:16 +0300

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Todo App';
+  todoArray = [];
+
+  deleteTodo(todo: string) {
+    for(let i = 0; i <= this.todoArray.length; i++) {
+      if(todo == this.todoArray[i]) {
+        this.todoArray.splice(i,1);
+      }
+    }
+  }
+
+  addTodo(value: string) {
+    if(value != "") {
+      this.todoArray.push(value);
+    }
+  }
+}
